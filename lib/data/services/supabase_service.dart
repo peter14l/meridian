d735c6937e772044d93ed9702b8f47fda8f706cc
PeterParker2008 <shreyas-sengupta2008@outlook.com>
2026-03-27 01:51:14 -0700

    initiatet supabase

--- lib/data/services/supabase_service.dart
+++ lib/data/services/supabase_service.dart
@@ -3,8 +3,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseService {
   static Future<void> initialize() async {
     // TODO: Replace with environment variables or user configuration
-    const supabaseUrl = 'YOUR_SUPABASE_URL';
-    const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+    const supabaseUrl = 'https://epudlrzohcgztuelyfmy.supabase.co';
+    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwdWRscnpvaGNnenR1ZWx5Zm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NTg5NDEsImV4cCI6MjA5MDEzNDk0MX0.SDJ_fmn0QfCjdXvBj5QaMYKG7YFi22GECJoLqgMdXFs';
 
     await Supabase.initialize(
       url: supabaseUrl,
